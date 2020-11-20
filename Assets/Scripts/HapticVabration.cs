84b5e6e98d86ec43cb8ebd46136ad53e80be3d51
Tal Sznicer <talsznicer@gmail.com>
2020-11-20 14:09:18 +0100

    added HapticVabration script

---
+++ Assets/Scripts/HapticVabration.cs
@@ -0,0 +1,58 @@
+﻿using System; // Added
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq; // Added
+using UnityEngine;
+using UnityEngine.UI; // Added
+
+public class HapticVabration : MonoBehaviour
+{
+     // Use this for initialization
+    void Start ()
+    {
+        Vibration.Init ();
+        Debug.Log ( "Application.isMobilePlatform: " + Application.isMobilePlatform );
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == Camera.main.gameObject)
+        {
+            Debug.Log("In");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == Camera.main.gameObject)
+        {
+            Debug.Log("Out");
+        }
+    }
+
+    public void TapVibrate ()
+    {
+        Vibration.Vibrate ();
+    }
+
+    public void TapCancelVibrate ()
+    {
+
+        Vibration.Cancel ();
+    }
+
+    public void TapPopVibrate ()
+    {
+        Vibration.VibratePop ();
+    }
+
+    public void TapPeekVibrate ()
+    {
+        Vibration.VibratePeek ();
+    }
+
+    public void TapNopeVibrate ()
+    {
+        Vibration.VibrateNope ();
+    }
+}
